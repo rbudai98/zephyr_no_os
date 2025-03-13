b85af90aef534833ea77731af9d0bc23d449e160
Robert Budai <robert.budai@analog.com>
2025-03-13 16:29:33 +0200

    example_max31875: update simplified i2c device retrieving
    Signed-off-by: Robert Budai <robert.budai@analog.com>

--- example_max31875/src/main.c
+++ example_max31875/src/main.c
@@ -28,17 +28,8 @@ int main(void)
 	struct max31875_dev* max31875_desc_ptr;
 
 	printk("Hello World! %s\n", CONFIG_BOARD_TARGET);
-	const struct device *TEST_DEVICE = DEVICE_DT_GET(DT_PARENT(DT_NODELABEL(max31875)));
 	
-	struct i2c_dt_spec TEST_DEVICE_FULL;
-	TEST_DEVICE_FULL.bus = TEST_DEVICE;
-	TEST_DEVICE_FULL.addr = DT_REG_ADDR(DT_NODELABEL(max31875));
-
-
-	if (!device_is_ready(TEST_DEVICE_FULL.bus)) {
-		printk("I2C bus device is not ready\n");
-		return -1;
-	}
+	struct i2c_dt_spec TEST_DEVICE_FULL = I2C_DT_SPEC_GET(DT_NODELABEL(max31875));
 
 	printk("I2C device addr: %d\n", TEST_DEVICE_FULL.addr);
 	zephyr_i2c_ip.i2c_desc = &TEST_DEVICE_FULL;
